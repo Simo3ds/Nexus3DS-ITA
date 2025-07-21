75e603b06f2f970f5d12dd3e2085ad78b0a27fd8
2b-zipper <pencil2b7777@gmail.com>
2025-07-21 15:34:48 +0900

    Fix config file generation by adding missing sprintf argument for plugin cache setting

--- arm9/source/config.c
+++ arm9/source/config.c
@@ -693,8 +693,9 @@ static size_t saveLumaIniConfigToStr(char *out)
         autobootModeStr,
 
         cfg->hbldr3dsxTitleId, rosalinaMenuComboStr, (int)(cfg->pluginLoaderFlags & 1),
-        (int)((cfg->pluginLoaderFlags & (1 << 1)) >> 1), (int)((cfg->pluginLoaderFlags & 1 << 2) >> 2),
+        (int)((cfg->pluginLoaderFlags & (1 << 1)) >> 1), (int)((cfg->pluginLoaderFlags & (1 << 2)) >> 2),
         (int)cfg->pluginWatcherLevel,
+        (int)((cfg->pluginLoaderFlags & (1 << 3)) >> 3),
         (int)cfg->ntpTzOffetMinutes,
 
         (int)cfg->topScreenFilter.cct, (int)cfg->bottomScreenFilter.cct,
