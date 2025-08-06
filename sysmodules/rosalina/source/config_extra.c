78d93f63cb9d96bec534571d92a23b71939dc30d
2b-zipper <pencil2b7777@gmail.com>
2025-08-06 18:03:01 +0900

    Fix text scrolling in Extra config and Plugin Loader settings

--- sysmodules/rosalina/source/config_extra.c
+++ sysmodules/rosalina/source/config_extra.c
@@ -9,9 +9,64 @@
 config_extra configExtra = { .suppressLeds = false, .cutSlotPower = false, .cutSleepWifi = false, .includeScreenshotTitleId = false };
 bool configExtraSaved = false;
 
+static int scrollOffset = 0;
+static u32 lastSelectedHash = 0;
+static int scrollDir = 1;
+static int scrollWait = 0;
+static const int scrollSpeed = 2;
+static const int scrollWaitFrames = 40;
+static const int scrollInitialWaitFrames = 15;
+
 static inline const char* ConfigExtra_GetCheckboxDisplay(bool value)
 {
     return value ? "(x)" : "( )";
+}
+
+static void ConfigExtra_DrawScrollableText(u32 xPos, u32 yPos, const char *text, bool selected)
+{
+    if (!selected) {
+        Draw_DrawString(xPos, yPos, COLOR_WHITE, text);
+        return;
+    }
+    
+    u32 currentHash = yPos ^ ((u32)text);
+
+    if (lastSelectedHash != currentHash) {
+        scrollOffset = 0;
+        lastSelectedHash = currentHash;
+        scrollDir = 1;
+        scrollWait = scrollInitialWaitFrames;
+    }
+    
+    int textLen = strlen(text);
+    int maxTextChars = 32;
+    
+    if (textLen > maxTextChars) {
+        int maxOffset = (textLen - maxTextChars) * 8;
+        
+        if (scrollWait > 0) {
+            scrollWait--;
+        } else {
+            scrollOffset += scrollSpeed * scrollDir;
+            if (scrollDir == 1 && scrollOffset >= maxOffset) {
+                scrollOffset = maxOffset;
+                scrollWait = scrollWaitFrames;
+                scrollDir = -1;
+            } else if (scrollDir == -1 && scrollOffset <= 0) {
+                scrollOffset = 0;
+                scrollWait = scrollWaitFrames;
+                scrollDir = 1;
+            }
+        }
+        
+        char buf[33];
+        int startChar = scrollOffset / 8;
+        strncpy(buf, text + startChar, maxTextChars);
+        buf[maxTextChars] = '\0';
+        Draw_DrawString(xPos, yPos, COLOR_CYAN, buf);
+    } else {
+        Draw_DrawString(xPos, yPos, COLOR_CYAN, text);
+    }
 }
 
 void ConfigExtra_DrawDetailedMenu(void)
@@ -39,20 +94,25 @@ void ConfigExtra_DrawDetailedMenu(void)
 
         for (s32 i = 0; i < nbOptions; i++) {
             u32 yPos = 40 + i * SPACING_Y;
-            char buf[128];
             const char *checkbox = ConfigExtra_GetCheckboxDisplay(*configValues[i]);
-            sprintf(buf, "%s %s", checkbox, configOptions[i]);
-            Draw_DrawMenuCursor(yPos, (i == selected), buf);
+            
+            if (i == selected) {
+                Draw_DrawString(15, yPos, COLOR_ORANGE, ">>");
+                Draw_DrawString(250, yPos, COLOR_ORANGE, "<<        ");
+                Draw_DrawString(35, yPos, COLOR_CYAN, checkbox);
+                ConfigExtra_DrawScrollableText(59, yPos, configOptions[i], true);
+            } else {
+                Draw_DrawString(15, yPos, COLOR_GRAY, " *");
+                Draw_DrawString(250, yPos, COLOR_WHITE, "  ");
+                Draw_DrawString(35, yPos, COLOR_WHITE, checkbox);
+                Draw_DrawString(59, yPos, COLOR_WHITE, configOptions[i]);
+            }
         }
         
         Draw_FlushFramebuffer();
         Draw_Unlock();
         
-        u32 pressed;
-        do
-        {
-            pressed = waitInputWithTimeout(50);
-        } while(pressed == 0 && !menuShouldExit);
+        u32 pressed = waitInputWithTimeout(30);
         
         if(pressed & KEY_B)
             break;
