12cf761275de0eddd33aca9bdd13dd8c9967a091
2b-zipper <pencil2b7777@gmail.com>
2025-08-26 18:21:55 +0900

    Add enhanced features list to README, add hide0123 to credits

--- sysmodules/rosalina/source/menus.c
+++ sysmodules/rosalina/source/menus.c
@@ -369,7 +369,7 @@ void RosalinaMenu_ShowCredits(void)
                 "  other people\n\n"
                 "People who made this fork possible:\n"
                 "  DullPointer, Tekito-256, Gruetzig, cooolgamer,\n"
-                "  JBMagination2, Pixel-Pop, 2b-zipper"
+                "  JBMagination2/hide0123, Pixel-Pop, 2b-zipper"
             ));
 
         Draw_FlushFramebuffer();
