036b0c008be48691c2bb1d38c329950d3a560898
2b-zipper <pencil2b7777@gmail.com>
2025-09-22 01:09:45 +0900

    Rewrite extra config menu

--- sysmodules/rosalina/source/config_extra.c
+++ sysmodules/rosalina/source/config_extra.c
@@ -17,9 +17,20 @@ static const int scrollSpeed = 2;
 static const int scrollWaitFrames = 40;
 static const int scrollInitialWaitFrames = 15;
 
+typedef struct {
+    const char *displayText;
+    bool *configValue;
+    bool (*visibility)(void);
+} ConfigMenuEntry;
+
 static inline const char* ConfigExtra_GetCheckboxDisplay(bool value)
 {
     return value ? "(x)" : "( )";
+}
+
+static bool CheckNotO2DS(void)
+{
+    return mcuInfoTableRead && mcuInfoTable[9] != 3;
 }
 
 static void ConfigExtra_DrawScrollableText(u32 xPos, u32 yPos, const char *text, bool selected)
@@ -71,50 +82,60 @@ static void ConfigExtra_DrawScrollableText(u32 xPos, u32 yPos, const char *text,
 
 void ConfigExtra_DrawDetailedMenu(void)
 {
-    static const char *configOptions[] = {
-        "Automatically suppress LEDs",
-        "Cut power to TWL Flashcards", 
-        "Cut 3DS Wifi in sleep mode",
-        "Include title ID in screenshot filename",
-        "Save screenshots in date folders",
-        "Combine top/bottom screenshots",
-        "Toggle bot LCD (start+select)(N/A on o2DS)",
-        "Use Fahrenheit for temperature display"
+    static const ConfigMenuEntry configMenuEntries[] = {
+        {"Automatically suppress LEDs", &configExtra.suppressLeds, NULL},
+        {"Cut power to TWL Flashcards", &configExtra.cutSlotPower, NULL},
+        {"Cut 3DS Wifi in sleep mode", &configExtra.cutSleepWifi, NULL},
+        {"Include title ID in screenshot filename", &configExtra.includeScreenshotTitleId, NULL},
+        {"Save screenshots in date folders", &configExtra.screenshotDateFolders, NULL},
+        {"Combine top/bottom screenshots", &configExtra.screenshotCombined, NULL},
+        {"Toggle bottom LCD backlight (start+select)", &configExtra.toggleLcdCombo, &CheckNotO2DS},
+        {"Use Fahrenheit for temperature display", &configExtra.temperatureUnit, NULL}
     };
-    
-    bool *configValues[] = {
-        &configExtra.suppressLeds,
-        &configExtra.cutSlotPower,
-        &configExtra.cutSleepWifi,
-        &configExtra.includeScreenshotTitleId,
-        &configExtra.screenshotDateFolders,
-        &configExtra.screenshotCombined,
-        &configExtra.toggleLcdCombo,
-        &configExtra.temperatureUnit
-    };
-    
+
     s32 selected = 0;
-    s32 nbOptions = sizeof(configOptions) / sizeof(const char *);
+    s32 nbOptions = sizeof(configMenuEntries) / sizeof(ConfigMenuEntry);
+
+    s32 visibleCount = 0;
+    s32 visibleMapping[sizeof(configMenuEntries) / sizeof(ConfigMenuEntry)];
     
+    for (s32 i = 0; i < nbOptions; i++) {
+        if (configMenuEntries[i].visibility == NULL || configMenuEntries[i].visibility()) {
+            visibleMapping[visibleCount] = i;
+            visibleCount++;
+        }
+    }
+
+    if (visibleCount == 0) return;
+
     do {
         Draw_Lock();
         Draw_DrawMenuFrame("Extra config menu");
 
+        s32 displayIndex = 0;
         for (s32 i = 0; i < nbOptions; i++) {
-            u32 yPos = 40 + i * SPACING_Y;
-            const char *checkbox = ConfigExtra_GetCheckboxDisplay(*configValues[i]);
-            
-            if (i == selected) {
+            const ConfigMenuEntry *entry = &configMenuEntries[i];
+
+            if (entry->visibility != NULL && !entry->visibility()) {
+                continue;
+            }
+
+            u32 yPos = 40 + displayIndex * SPACING_Y;
+            const char *checkbox = ConfigExtra_GetCheckboxDisplay(*(entry->configValue));
+
+            if (displayIndex == selected) {
                 Draw_DrawString(15, yPos, COLOR_ORANGE, ">>");
                 Draw_DrawString(250, yPos, COLOR_ORANGE, "<<        ");
                 Draw_DrawString(35, yPos, COLOR_CYAN, checkbox);
-                ConfigExtra_DrawScrollableText(59, yPos, configOptions[i], true);
+                ConfigExtra_DrawScrollableText(59, yPos, entry->displayText, true);
             } else {
                 Draw_DrawString(15, yPos, COLOR_GRAY, " *");
                 Draw_DrawString(250, yPos, COLOR_WHITE, "  ");
                 Draw_DrawString(35, yPos, COLOR_WHITE, checkbox);
-                Draw_DrawString(59, yPos, COLOR_WHITE, configOptions[i]);
+                Draw_DrawString(59, yPos, COLOR_WHITE, entry->displayText);
             }
+
+            displayIndex++;
         }
         
         Draw_FlushFramebuffer();
@@ -126,7 +147,9 @@ void ConfigExtra_DrawDetailedMenu(void)
             break;
         else if(pressed & KEY_A)
         {
-            *configValues[selected] = !(*configValues[selected]);
+            s32 actualIndex = visibleMapping[selected];
+            bool *selectedValue = configMenuEntries[actualIndex].configValue;
+            *selectedValue = !(*selectedValue);
             LumaConfig_RequestSaveSettings();
         }
         else if(pressed & KEY_DOWN)
@@ -135,8 +158,8 @@ void ConfigExtra_DrawDetailedMenu(void)
             selected--;
             
         if(selected < 0)
-            selected = nbOptions - 1;
-        else if(selected >= nbOptions)
+            selected = visibleCount - 1;
+        else if(selected >= visibleCount)
             selected = 0;
             
     } while(!menuShouldExit);
